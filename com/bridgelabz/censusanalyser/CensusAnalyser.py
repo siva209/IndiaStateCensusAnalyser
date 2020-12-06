022f5614d25b394e09ce7066e903debe017ac8e4
siva209 <sivanarayanayeruva209@gmail.com>
2020-12-06 17:26:54 +0530

    [Added:Refactored the code using DRY principle]

--- com/bridgelabz/censusanalyser/CensusAnalyser.py
+++ com/bridgelabz/censusanalyser/CensusAnalyser.py
@@ -1,32 +1,23 @@
 import pandas as pd
 
-from com.bridgelabz.censusanalyser.CensusAnalyserStateCode import CensusAnalyserStateCode
 from com.bridgelabz.censusanalyser.CensusAnalysisException import CensusAnalysisException
-from com.bridgelabz.censusanalyser.CensusAnalysisHeader import CensusAnalysisHeader
-
-
 
 
 class CensusAnalyzer:
 
-    def __init__(self, path):
+    def __init__(self, path, obj):
         self.path = path
+        self.obj = obj
 
-    def load_state_code(self):
+    def record_counter(self):
         """
-        :return: number of records in file
-        """
+            :return: number of records in file
+            """
         try:
-            col_names = repr(CensusAnalyserStateCode()).split(",")
+            col_names = repr(self.obj).split(",")
             data = pd.read_csv(self.path, usecols=col_names)
             return len(data)
         except FileNotFoundError:
             raise CensusAnalysisException("Invalid File Path")
         except ValueError:
             raise CensusAnalysisException("Wrong Delimiter or Invalid Columns Name")
-
-
-if __name__ == "__main__":
-    read_csv = CensusAnalyzer(path="C:/Users/SRS/Downloads/IndiaStateCensusAnalyser/Data/IndiaStateCode .csv")
-    length = read_csv.load_state_code()
-    print(length)
