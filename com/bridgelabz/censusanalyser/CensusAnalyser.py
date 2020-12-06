283b4d7498a88975ef2b0ce07ef98e4b3b8115eb
siva209 <sivanarayanayeruva209@gmail.com>
2020-12-06 16:37:37 +0530

    [Added:Resolved Conflicts in indiastatcode]

--- com/bridgelabz/censusanalyser/CensusAnalyser.py
+++ com/bridgelabz/censusanalyser/CensusAnalyser.py
@@ -1,6 +1,10 @@
 import pandas as pd
+
+from com.bridgelabz.censusanalyser.CensusAnalyserStateCode import CensusAnalyserStateCode
 from com.bridgelabz.censusanalyser.CensusAnalysisException import CensusAnalysisException
 from com.bridgelabz.censusanalyser.CensusAnalysisHeader import CensusAnalysisHeader
+
+
 
 
 class CensusAnalyzer:
@@ -8,12 +12,12 @@ class CensusAnalyzer:
     def __init__(self, path):
         self.path = path
 
-    def record_counter(self):
+    def load_state_code(self):
         """
         :return: number of records in file
         """
         try:
-            col_names = repr(CensusAnalysisHeader()).split(",")
+            col_names = repr(CensusAnalyserStateCode()).split(",")
             data = pd.read_csv(self.path, usecols=col_names)
             return len(data)
         except FileNotFoundError:
@@ -23,6 +27,6 @@ class CensusAnalyzer:
 
 
 if __name__ == "__main__":
-    read_csv = CensusAnalyzer(path="C:/Users/SRS/Downloads/IndiaStateCensusAnalyser/Data/IndiaStateCensusData.csv")
-    length = read_csv.record_counter()
+    read_csv = CensusAnalyzer(path="C:/Users/SRS/Downloads/IndiaStateCensusAnalyser/Data/IndiaStateCode .csv")
+    length = read_csv.load_state_code()
     print(length)
